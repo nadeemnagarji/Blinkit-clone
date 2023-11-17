daebe9c9f0ed7549a6b2def212ababc434e1f9da
Nadeem <nadeemnagarji@gmail.com>
2023-11-17 14:48:33 +0530

    made responsive

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log(45)
+const phoneNav = document.getElementById("phone-nav")
+const PcNav = document.getElementById("big-nav")
+
+
+function togglenavbar(){
+    if(window.innerWidth<900){
+        PcNav.style.display="none"
+    }else{
+        PcNav.style.display="flex"
+    }
+}
+
+window.addEventListener("resize",togglenavbar)
